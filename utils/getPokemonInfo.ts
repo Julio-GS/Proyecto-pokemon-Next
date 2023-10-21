f2a5beee6b28f16dcb8bce21da8aacae2b7a7590
Julio-Gomez <juliocesargomezsena@gmail.com>
2023-10-20 21:59:22 -0300

    fin seccion 6, implementacion ISR-ISG

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,11 +2,17 @@ import { pokeApi } from "@/api";
 import { Pokemon } from "@/interfaces";
 
 export const getPokemonInfo = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+  const url = nameOrId.toLowerCase();
 
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${url}`);
+
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites,
+    };
+  } catch (error) {
+    return null;
+  }
 };
